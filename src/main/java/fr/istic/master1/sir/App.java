cc305ab61d0431dafdd86fae16638a9e067e5787
ines-beugre <ines_beugre@yahoo.fr>
2018-09-13 22:07:51 +0200

    first commit of maven work

---
+++ src/main/java/fr/istic/master1/sir/App.java
@@ -0,0 +1,17 @@
+package fr.istic.master1.sir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public String yourMessage(){
+        String message = "hello";
+        return message;
+    }
+
+}
